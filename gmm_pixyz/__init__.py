199a751eff4260e94db91d8962695875f7faf231
kuniyasu <k1512051@edu.cc.uec.ac.jp>
2019-07-17 16:48:31 +0900

    pixyz版VAE, GMM追加

---
+++ gmm_pixyz/__init__.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append( "../" )
+
+import serket as srk
+import numpy as np
+import torch
+from pixyz.distributions import Normal, Categorical
+from pixyz.distributions.mixture_distributions import MixtureModel 
+import os
+from copy import deepcopy
+
+class GMM(srk.Module):
+    def __init__( self, K, itr=100, name="gmm", category=None, mode="learn" ):
+        super(GMM, self).__init__(name, True)
+        self.__K = K
+        self.__itr = itr
+        self.__category = category
+        self.__mode = mode
+        self.__save_dir = self.get_name()
+            
+        if mode != "learn" and mode != "recog":
+            raise ValueError("choose mode from \"learn\" or \"recog\"")
+        
+        if torch.cuda.is_available():
+            self.__device = "cuda"
+        else:
+            self.__device = "cpu"
+
+    def build_model( self ):
+        # P(x|z)の作成
+        self.__distributions = []
+        for i in range(self.__K):
+            loc = torch.randn( self.__input_dim ).to(self.__device)
+            scale = torch.empty( self.__input_dim ).fill_(0.5).to(self.__device)
+            self.__distributions.append( Normal(loc=loc, scale=scale, var=["x"], name="p_%d" %i).to(self.__device) )
+        
+        # P(z)の作成
+        self.__prior = Categorical( probs=self.__prior_probs, var=["z"], name="p_{prior}" ).to(self.__device)
+        
+        # P(x)の作成
+        self.__p = MixtureModel( distributions=self.__distributions, prior=self.__prior ).to(self.__device)
+        
+        # P(z|x)の作成        
+        self.__post = self.__p.posterior()
+        
+    def train( self ):
+        # E-step
+        self.__posterior = self.__post.prob().eval({"x": self.__data[0]})
+        
+        # M-step
+        N_k = self.__posterior.sum(dim=1)
+        
+        # update probs
+        probs = N_k / N_k.sum()
+        self.__prior.probs[0] = probs
+
+        # update loc & scale
+        loc = (self.__posterior[:, None] @ self.__data[0][None]).squeeze(1)
+        loc /= (N_k[:, None] + 1e-6)
+    
+        cov = (self.__data[0][None, :, :] - loc[:, None, :]) ** 2
+        var = (self.__posterior[:, None, :] @ cov).squeeze(1)
+        var /= (N_k[:, None] + 1e-6)
+        scale = var.sqrt()
+    
+        for i, d in enumerate(self.__distributions):
+            d.loc[0] = loc[i]
+            d.scale[0] = scale[i]
+
+    def sampling( self ):
+        # 事後分布からサンプリングできるように転置・カテゴリカル分布の生成
+        self.__post_probs = torch.t( self.__posterior )
+        class_dist = Categorical( probs=self.__post_probs )
+        
+        # classをone-hotからintに変換
+        one_hot_class = class_dist.sample()["x"].cpu().detach().numpy()[0]
+        self.__classes = [np.where(r==1)[0][0] for r in one_hot_class]
+        
+    def calc_acc( self ):
+        self.__max_acc = 0  # 精度の最大値
+        changed = True      # 変化したかどうか
+        self.__rp_classes = deepcopy( self.__classes ) # replace用にclassesをコピー
+    
+        while changed:
+            changed = False
+            for i in range(self.__K):
+                for j in range(self.__K):
+                    tmp_result = np.zeros( self.__N )
+    
+                    # iとjを入れ替える
+                    for n in range(self.__N):
+                        if self.__rp_classes[n]==i: tmp_result[n]=j
+                        elif self.__rp_classes[n]==j: tmp_result[n]=i
+                        else: tmp_result[n] = self.__rp_classes[n]
+    
+                    # 精度を計算
+                    acc = (tmp_result==self.__category).sum() / float(self.__N)
+    
+                    # 精度が高くなって入れば保存
+                    if acc > self.__max_acc:
+                        self.__max_acc = acc
+                        self.__rp_classes = tmp_result
+                        changed = True
+    
+    def save_model( self ):
+        if not os.path.exists( self.__save_dir ):
+            os.mkdir( self.__save_dir )
+    
+        # モデルパラメータの保存
+        if self.__mode == "learn":
+            # 確率と平均の保存
+            loc = []
+            scale = []
+            for k in range(self.__K):
+                loc.append( self.__distributions[k].loc[0].cpu().detach().numpy() )
+                scale.append( self.__distributions[k].scale[0].cpu().detach().numpy() )
+            np.savetxt( os.path.join( self.__save_dir, "mu.txt" ), loc, fmt=str("%f") )
+            np.savetxt( os.path.join( self.__save_dir, "sigma.txt" ), scale, fmt=str("%f") )
+    
+        # 分類結果・精度の計算・保存
+        if self.__category is not None:
+            self.calc_acc()
+            np.savetxt( os.path.join( self.__save_dir, "class_{}.txt".format(self.__mode) ), self.__rp_classes, fmt=str("%d") )
+            np.savetxt( os.path.join( self.__save_dir, "acc_{}.txt".format(self.__mode) ), [self.__max_acc], fmt=str("%f") )
+            
+        else:
+            np.savetxt( os.path.join( self.__save_dir, "class{}.txt".format(self.__mode) ), self.__classes, fmt=str("%d") )
+
+    def load_model( self ):
+        loc = np.loadtxt("mu.txt")
+        scale = np.loadtxt("sigma.txt")
+        for i, d in enumerate(self.__distributions):
+            d.loc[0] = loc[i].to(self.__device)
+            d.scale[0] = scale[i].to(self.__device)
+    
+    def recog( self ):
+        self.load_model()
+        self.__posterior = self.__post.prob().eval({"x": self.__data[0]})
+
+    def update( self, i ):
+        self.__data = self.get_observations()
+        self.__prior_probs = self.get_backward_msg()    # バックワードされた事前分布
+
+        self.__N = len(self.__data[0])                  # データ数
+        self.__input_dim = len( self.__data[0][0] )     # 入力の次元数
+        
+        # backward messageがまだ計算されていないとき
+        if self.__prior_probs is None:
+            self.__prior_probs = torch.empty( self.__K ).fill_( 1. / self.__K ).to(self.__device) # 一様分布
+
+        self.__data[0] = torch.Tensor( self.__data[0] ).to(self.__device)
+        
+        self.__save_dir = self.get_name()+"_%d"%i
+        
+        # VAEの構築
+        self.build_model()
+        
+        # 学習or認識
+        if self.__mode=="learn":
+            for i in range(self.__itr):
+                self.train()
+        else:
+            self.recog()
+        
+        # サンプリング
+        self.sampling()
+
+        # 結果を保存
+        self.save_model()
+
+        # メッセージの送信
+        self.set_forward_msg( self.__post )
+        self.send_backward_msgs( [[self.__distributions, self.__classes]] )
---
+++ vae_pixyz/__init__.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append( "../" )
+
+import serket as srk
+import numpy as np
+import torch
+from pixyz.distributions import Normal
+from pixyz.losses import KullbackLeibler, StochasticReconstructionLoss
+from pixyz.models import Model
+from abc import ABCMeta, abstractmethod
+import os
+
+class VAE(srk.Module, metaclass=ABCMeta):
+    def __init__( self, latent_dim, itr=5000, name="vae", batch_size=None, KL_param=1, mode="learn" ):
+        super(VAE, self).__init__(name, True)
+        self.__latent_dim = latent_dim
+        self.__itr = itr
+        self.__batch_size = batch_size
+        self.__KL_param = KL_param
+        self.__mode = mode
+            
+        if mode != "learn" and mode != "recog":
+            raise ValueError("choose mode from \"learn\" or \"recog\"")
+        
+        if torch.cuda.is_available():
+            self.__device = "cuda"
+        else:
+            self.__device = "cpu"
+
+    @abstractmethod
+    def network( self, input_dim, latent_dim ):
+        class Inference(Normal):
+            def __init__( self ):
+                pass
+            
+            def forward( self, x ):
+                pass
+        
+        class Generator(Normal):
+            def __init__( self ):
+                pass
+            
+            def forward( self, z ):
+                pass
+
+    def build_model(self):
+        class Prior(Normal):
+            def __init__( self ):
+                super(Prior, self).__init__(cond_var=["mu", "sigma"], var=["x"], name="p_{prior}")
+                
+            def forward(self, mu, sigma):
+                return {"loc": mu, "scale": sigma}
+        
+        # priorの生成
+        prior = Prior()
+        
+        # network構造の取得
+        self.__q, self.__p, optimizer, op_param = self.network( self.__input_dim, self.__latent_dim )
+        
+        # lossの定義
+        loss = ( self.__KL_param*KullbackLeibler(self.__q, prior) + StochasticReconstructionLoss(self.__q, self.__p) ).mean()
+        
+        # modelの構築
+        self.__model = Model( loss=loss, distributions=[self.__p, self.__q], optimizer=optimizer, optimizer_params=op_param )
+        
+    def train( self ):
+        # loss保存用のlist
+        self.__loss_save = []
+        
+        # 学習
+        if self.__mode == "learn":
+            for step in range(1, self.__itr+1):
+                # バッチ学習
+                if self.__batch_size==None:
+                    cur_loss = self.__model.train( {"x": self.__data[0], "mu": self.__mu, "sigma": self.__sigma} )
+                    # 50ごとにloss保存
+                    if step % 50 == 0:
+                        self.__loss_save.append( [step, cur_loss] )
+            
+                # ミニバッチ学習
+                else:                
+                    sff_idx = np.random.permutation(self.__N)
+                    for idx in range(0, self.__N, self.__batch_size):
+                        batch = self.__data[0][sff_idx[idx: idx + self.__batch_size if idx + self.__batch_size < self.__N else self.__N]]
+                        batch_mu = self.__mu[sff_idx[idx: idx + self.__batch_size if idx + self.__batch_size < self.__N else self.__N]]
+                        batch_sigma = self.__sigma[sff_idx[idx: idx + self.__batch_size if idx + self.__batch_size < self.__N else self.__N]]
+                        cur_loss = self.__model.train( {"x": batch, "mu": batch_mu, "sigma": batch_sigma} )
+                    # epochごとにloss保存
+                    self.__loss_save.append( [step, cur_loss] )
+                    
+            # encoder保存
+            torch.save( self.__q.state_dict(), os.path.join(self.__save_dir, "encoder") )
+            # decoder保存
+            torch.save( self.__p.state_dict(), os.path.join(self.__save_dir, "decoder") )
+               
+        # 認識モード時はモデルの読み込み
+        if self.__mode == "recog":
+            # encoderの読み込み
+            self.__q.load_state_dict( torch.load( os.path.join(self.__save_dir, "encoder") ) )
+            # decoderの読み込み
+            self.__p.load_state_dict( torch.load( os.path.join(self.__save_dir, "decoder") ) )
+        
+        # サンプリング
+        self.__zz = self.__q.sample( self.__data[0], return_all=False )
+        self.__xx = self.__p.sample_mean( self.__zz )
+        # ndarrayに変換
+        self.__zz = self.__zz["z"].cpu().numpy()
+        self.__xx = self.__xx.cpu().detach().numpy()
+
+    def save_result( self ):
+        # ｚ,x_hatを保存
+        np.savetxt( os.path.join( self.__save_dir, "z_{}.txt".format(self.__mode) ), self.__zz )
+        np.savetxt( os.path.join( self.__save_dir, "x_hat_{}.txt".format(self.__mode) ), self.__xx )
+        
+        # 学習モード時はlossを保存
+        if self.__mode == "learn":
+            np.savetxt( os.path.join( self.__save_dir, "loss.txt" ), self.__loss_save )
+
+    def update( self, i ):
+        self.__data = self.get_observations()
+        back_msg = self.get_backward_msg()              # [Nomals, calasses]
+
+        self.__N = len(self.__data[0])                  # データ数
+        
+        if len(self.__data[0].shape)==2:
+            self.__input_dim = self.__data[0][0].shape  # 入力の次元数
+        else:
+            self.__input_dim = self.__data[0][0].shape  # 時系列データ(系列長*次元数)or画像の場合(高さ*幅*チャンネル)
+        
+        # backward messageがまだ計算されていないとき
+        if back_msg is None:
+            self.__mu = np.zeros( (self.__N, self.__latent_dim) )
+            self.__sigma = np.ones( (self.__N, self.__latent_dim) )
+        else:
+            mus = [ back_msg[0][i].loc[0] for i in range(len(back_msg[0])) ]
+            sigmas = [ back_msg[0][i].scale[0] for i in range(len(back_msg[0])) ]
+            self.__mu = [ mus[c].cpu().detach().numpy() for c in back_msg[1] ]
+            self.__sigma = [ sigmas[c].cpu().detach().numpy() for c in back_msg[1] ]
+        
+        self.__data[0] = torch.Tensor( self.__data[0] ).to(self.__device)
+        self.__mu = torch.Tensor( self.__mu ).to(self.__device)
+        self.__sigma = torch.Tensor( self.__sigma ).to(self.__device)
+
+        self.__save_dir = self.get_name()+"_%d"%i
+        
+        # 保存フォルダの作成
+        if not os.path.exists( self.__save_dir ):
+            os.mkdir( self.__save_dir )
+        
+        # VAEの構築・学習
+        self.build_model()
+        self.train()
+
+        # 結果を保存
+        self.save_result()
+
+        # メッセージの送信
+        self.set_forward_msg( self.__zz )
+        self.send_backward_msgs( [self.__xx] )
