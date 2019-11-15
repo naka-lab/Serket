e78ca532850fb9e5ab6438096a95709b8e9605fd
kuniyasu <k1932034@edu.cc.uec.ac.jp>
2019-11-15 19:02:15 +0900

    修正

--- examples/VAE-GMM-MLDA-MM/VAE-GMM-MLDA-MM.py
+++ examples/VAE-GMM-MLDA-MM/VAE-GMM-MLDA-MM.py
@@ -10,7 +10,27 @@ import mlda
 import mm
 import serket as srk
 import numpy as np
+import tensorflow as tf
 
+encoder_dim = 128
+decoder_dim = 128
+
+class vae_model(vae.VAE):
+    def build_encoder(self, x, latent_dim):
+        h_encoder = tf.keras.layers.Dense(encoder_dim, activation="relu")(x)
+
+        mu = tf.keras.layers.Dense(latent_dim)(h_encoder)
+        logvar = tf.keras.layers.Dense(latent_dim)(h_encoder)
+        
+        return mu, logvar
+    
+    def build_decoder(self, z):
+        h_decoder = tf.keras.layers.Dense(decoder_dim, activation="relu")(z)
+        logits = tf.keras.layers.Dense(784)(h_decoder)
+
+        optimizer = tf.train.AdamOptimizer()
+        
+        return logits, optimizer
 
 def main():
     obs1 = srk.Observation( np.loadtxt("mnistdata.txt") )
@@ -18,9 +38,9 @@ def main():
     
     category = np.loadtxt("label.txt")
     
-    vae1 = vae.VAE(15, itr=150, batch_size=500)
+    vae1 = vae_model(10, itr=200, batch_size=500)
     gmm1 = gmm.GMM(10, category=category)
-    mlda1 = mlda.MLDA(10, [200,200], category=category)
+    mlda1 = mlda.MLDA(10, [500,500], category=category)
     mm1 = mm.MarkovModel()
     
     vae1.connect( obs1 )
@@ -28,16 +48,13 @@ def main():
     mlda1.connect( obs2, gmm1 )
     mm1.connect( mlda1 )
     
-    for it in range(5):
-        print( it )
+    for i in range(5):
+        print( i )
         vae1.update()
         gmm1.update()
         mlda1.update()
         mm1.update()
-    
-
 
 if __name__=="__main__":
     main()
-    
     
