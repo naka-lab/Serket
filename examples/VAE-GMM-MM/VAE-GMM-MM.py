ed8bccffa643dee10416a77646e7b31daf0f50bf
kuniyasu <k1932034@edu.cc.uec.ac.jp>
2019-11-15 19:04:16 +0900

    修正

--- examples/VAE-GMM-MM/VAE-GMM-MM.py
+++ examples/VAE-GMM-MM/VAE-GMM-MM.py
@@ -9,13 +9,33 @@ import gmm
 import mm
 import serket as srk
 import numpy as np
+import tensorflow as tf
 
+encoder_dim = 128
+decoder_dim = 128
+
+class vae_model(vae.VAE):
+    def build_encoder(self, x, latent_dim):
+        h_encoder = tf.keras.layers.Dense(encoder_dim, activation="relu")(x)
+
+        mu = tf.keras.layers.Dense(latent_dim)(h_encoder)
+        logvar = tf.keras.layers.Dense(latent_dim)(h_encoder)
+        
+        return mu, logvar
+    
+    def build_decoder(self, z):
+        h_decoder = tf.keras.layers.Dense(decoder_dim, activation="relu")(z)
+        logits = tf.keras.layers.Dense(784)(h_decoder)
+
+        optimizer = tf.train.AdamOptimizer()
+        
+        return logits, optimizer
 
 def main():
     obs1 = srk.Observation( np.loadtxt("mnistdata.txt") )
     category = np.loadtxt("mnistlabel.txt")
     
-    vae1 = vae.VAE(15, itr=200, batch_size=500)
+    vae1 = vae_model(10, itr=200, batch_size=500)
     gmm1 = gmm.GMM(10, category=category)
     mm1 = mm.MarkovModel()
     
@@ -23,15 +43,12 @@ def main():
     gmm1.connect( vae1 )
     mm1.connect( gmm1 )
     
-    for it in range(5):
-        print( it )
+    for i in range(5):
+        print( i )
         vae1.update()
         gmm1.update()
         mm1.update()
-    
-
 
 if __name__=="__main__":
     main()
-    
     
