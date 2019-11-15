27aaac0a8ef1597b9b279395a99d4f67ec33b18f
kuniyasu <k1932034@edu.cc.uec.ac.jp>
2019-11-15 18:57:13 +0900

    修正

--- gmm/__init__.py
+++ gmm/__init__.py
@@ -9,16 +9,13 @@ import numpy as np
 import os
 
 class GMM(srk.Module):
-    def __init__( self, K, itr=100, name="gmm", category=None, mode="learn" ):
+    def __init__( self, K, itr=100, name="gmm", category=None, load_dir=None ):
         super(GMM, self).__init__(name, True)
         self.__K = K
         self.__itr = itr
         self.__category = category
-        self.__mode = mode
+        self.__load_dir = load_dir
         self.__n = 0
-        
-        if mode != "learn" and mode != "recog":
-            raise ValueError("choose mode from \"learn\" or \"recog\"")
 
     def update(self):
         data = self.get_observations()
@@ -32,9 +29,13 @@ class GMM(srk.Module):
 
         data[0] = np.array( data[0], dtype=np.float32 )
 
-        save_dir = os.path.join( self.get_name(), "%03d" % self.__n )
+        if self.__load_dir is None:
+            save_dir = os.path.join( self.get_name(), "%03d" % self.__n )
+        else:
+            save_dir = os.path.join( self.get_name(), "recog" )
+
         # GMM学習
-        Pdz, mu = gmm.train( data[0], self.__K, self.__itr, save_dir, Pdz, self.__category, self.__mode )
+        Pdz, mu = gmm.train( data[0], self.__K, self.__itr, save_dir, Pdz, self.__category, self.__load_dir )
         
         self.__n += 1
 
